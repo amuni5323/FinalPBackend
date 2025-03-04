e118265878135a29fd0fb3f496f96584ff0a3279
amuni5323 <amuni11.hamai@gmail.com>
2025-03-04 12:41:55 +0300

    updated

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -25,7 +25,7 @@ const sendEmail = (to, subject, htmlContent) => {
 
 // Specific function for verification email
 const sendVerificationEmail = (email, token) => {
-  const verificationLink = `http://localhost:5000/api/users/verify/${token}`;
+  const verificationLink = `https://final-p-frontend-vkim.vercel.app/api/users/verify/${token}`;
   
   const emailContent = `
     <p>Click the link to verify your email: <a href="${verificationLink}">${verificationLink}</a></p>
